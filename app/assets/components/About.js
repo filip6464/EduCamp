e63002e308bdbccd5b3e002182c92096c50acb70
Filip6464 <filip-zyga@wp.pl>
2021-03-26 18:41:28 +0100

    about & contact plate components

--- app/assets/components/About.js
+++ app/assets/components/About.js
@@ -7,16 +7,22 @@ import {makeStyles} from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
     root: {
-
-        padding: '1rem',
+        margin: '2rem',
+        padding: '2.5rem',
         marginTop: '0.8rem',
         marginBottom: '1rem',
         minWidth: '45%',
-        minHeight: '80%',
+        minHeight: '55vh',
         maxWidth: '45%',
-        maxHeight: '80%',
+        maxHeight: '70vh',
+    },
+    title:{
+        marginBottom: '2rem'
 
     },
+    description:{
+
+    }
 });
 
 
@@ -24,10 +30,10 @@ export default function About() {
     const classes = useStyles();
     return (
             <Paper className={classes.root}>
-                <Typography gutterBottom variant="h5" component="h2">
+                <Typography className={classes.title} gutterBottom variant="h4" component="h2">
                     About page
                 </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
+                <Typography className={classes.description} variant="body2" color="textSecondary" component="p">
                     Some example text to show description of the homework. Some example text to show description of the homework. Some example text to show description of the homework.
                 </Typography>
             </Paper>
--- app/assets/pages/Contact.js
+++ app/assets/pages/Contact.js
@@ -4,14 +4,16 @@ import ContactForm from "../components/ContactForm";
 import {Box, Paper} from "@material-ui/core";
 import {Book} from "@material-ui/icons";
 import About from "../components/About";
+import ContactPlate from "../components/ContactPlate";
 
 class Contact extends Component {
     render() {
         return (
             <div>
                 <TitleBox title='Contact'/>
-                <Box component="div">
+                <Box component="div" display="flex" flexDirection="row">
                     <About />
+                    <ContactPlate />
                 </Box>
             </div>
         );
